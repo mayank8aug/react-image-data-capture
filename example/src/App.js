3d262a4c86a3d9de97d9e1c0b61c0803d5a7d847
mayank8aug <mayank8aug@gmail.com>
2021-03-30 23:24:47 +0530

    Updated example/

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
-import ImageCapture from 'react-image-capture'
-import 'react-image-capture/dist/index.css'
+import ImageCapture from 'react-image-data-capture';
 
 const App = () => {
-  const onCapture = imageData => { debugger };
-  const onError = error => { debugger };
+  const onCapture = imageData => { console.log(imageData) };
+  const onError = error => { console.log(error) };
   return <ImageCapture onCapture={onCapture} onError={onError} width={300} userMediaConfig={{ video: true }} />
 }
 
