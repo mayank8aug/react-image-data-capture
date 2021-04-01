37bd8ec77ce57ec681c3c407c43572c4ee3ea12f
mayank8aug <mayank8aug@gmail.com>
2021-04-01 20:34:01 +0530

    Updates

--- precache-manifest.68ba495081f3b286c850398782f0d952.js
+++ precache-manifest.68ba495081f3b286c850398782f0d952.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a7ff2fed83ec41e319f1bcbb5b377d32",
+    "revision": "0c8b34f212267555f215413428508fc8",
     "url": "./index.html"
   },
   {
-    "revision": "eeea27cf3ac4898a656b",
+    "revision": "1cde663a8ab1f783617d",
     "url": "./static/css/main.42cc8b22.chunk.css"
   },
   {
-    "revision": "a378eded3e0c7a1a161f",
-    "url": "./static/js/2.69b8145f.chunk.js"
+    "revision": "c6c4da74b6b772b9633a",
+    "url": "./static/js/2.36dd20e1.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "./static/js/2.69b8145f.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.36dd20e1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "eeea27cf3ac4898a656b",
-    "url": "./static/js/main.177e288e.chunk.js"
+    "revision": "1cde663a8ab1f783617d",
+    "url": "./static/js/main.96190175.chunk.js"
   },
   {
     "revision": "434f25f4749a37f1cce6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.10dd7bad65bf0489b7924aad86d4cf73.js"
+  "./precache-manifest.68ba495081f3b286c850398782f0d952.js"
 );
 
 self.addEventListener('message', (event) => {
